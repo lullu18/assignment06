da9e2b212bb9f9266990f6ec469a5be0a29bb912
lullu18 <limgayoon061108@gmail.com>
2025-11-17 13:21:54 +0900

    파티클을 연어(알)로 바꾸기

--- sketch.js
+++ sketch.js
@@ -1,12 +1,39 @@
-let system;
+let dividerY;
+let salmons = [];
+let system = [];
 
 function setup() {
-  createCanvas(720, 400);
-  system = new ParticleSystem(createVector(width / 2, 50));
+  createCanvas(800, 600);
+  dividerY = height / 2;
+
+  textAlign(CENTER, CENTER);
+  textSize(32);
 }
 
 function draw() {
-  background(51);
-  system.addParticle();
-  system.run();
+  background(20, 40, 80);
+
+  drawRiverZones();
+
+  // 연어 업데이트 & 그리기
+  for (let i = salmons.length - 1; i >= 0; i--) {
+    salmons[i].update();
+    salmons[i].show();
+
+    // 다 터지고 더 이상 쓸모 없으면 리스트에서 제거
+    if (salmons[i].finished) {
+      salmons.splice(i, 1);
+    }
+  }
+
+  // 파티클 시스템 업데이트 & 그리기
+  for (let i = systems.length - 1; i >= 0; i--) {
+    systems[i].run();
+    if (systems[i].isEmpty()) {
+      systems.splice(i, 1);
+    }
+  }
+
+  // 마우스를 따라다니는 연어 이모티콘
+  drawCursorSalmon();
 }
